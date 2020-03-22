dc9c8483ee1c16eb7262f8fd3845d8db29525eab
selamawit Ayalew <slmwt_assefa@yahoo.com>
2020-03-22 18:11:19 -0400

    Add files via upload

---
+++ constants.java
@@ -0,0 +1,15 @@
+package com.example.moviedirectory.Util;
+
+public class constants {
+
+    //Not free anymore - I am paying $1USD/month for access
+    //For now you can use this API Key, but please note that if too many people are using this
+    // API, the app may time-out.
+    //&apikey=cdc4cef5
+
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
+    public static final String URL = "http://www.omdbapi.com/?i=";
+    public static final String API_KEY = "tt3896198&apikey=38791d2a";
+
+    public static final String URL_RIGHT = "&page=2";
+}
